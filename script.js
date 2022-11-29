4c278d2fd562f3b64b12c5e6159a86aeebed8c78
lazycodermern <rupanchakravarthymern@gmail.com>
2022-11-29 12:39:52 +0530

    s

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-// class Car{
-//     constructor(brand ,color){
-//         this.brand = brand; // key = value
-//         this.color = color; 
-//     }
-//     printcolor(str){
-//     return str+this.color;
-// }
-// }
-// var test = new Car("Audi", "black");
-// var result = test.printcolor("the color of my car is ")
-// console.log(result);
+//example 1
+class Car{
+    constructor(brand ,color){
+        this.brand = brand; // key = value
+        this.color = color; 
+    }
+    printcolor(str){
+    return str+this.color;
+}
+}
+var test = new Car("Audi", "black");
+var result = test.printcolor("the color of my car is ")
+console.log(result);
+
+
+//example 2
 class Circle{
     constructor(radius , color){
         this.radius = radius;
